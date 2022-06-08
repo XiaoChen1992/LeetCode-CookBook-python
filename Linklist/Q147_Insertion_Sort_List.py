2cd44b8e05c3de562de7fd668d4836dfae00586a
Shawn Chen <kingchen92@gmail.com>
2022-06-08 17:42:42 -0400

    new solution

---
+++ Linklist/Q147_Insertion_Sort_List.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    @staticmethod
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev, cur = head, head.next
+
+        while cur:
+            if cur.val >= prev.val:
+                prev, cur = cur, cur.next
+
+            tmp = head
+            while cur.val > tmp.next.val:
+                tmp = tmp.next
+
+            prev.next = cur.next
+            cur.next = tmp.next
+            tmp.next = cur
+            cur = prev.next
+
+        return dummy.next
+
+
+if __name__ == '__main__':
+    node1 = ListNode(-1)
+    node2 = ListNode(5)
+    node3 = ListNode(3)
+    node4 = ListNode(4)
+    node5 = ListNode(0)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+
+    ans = Solution()
+    ans.insertionSortList(node1)
